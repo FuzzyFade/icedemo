d010e8319c11baeab6e626d88329084b81d6aab9
Fuzzy Fade <1339184537@qq.com>
2021-06-03 18:16:57 +0800

    feat: over

--- build.plugin.js
+++ build.plugin.js
@@ -1,24 +1,20 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const cfg = require('./config')
+const path = require('path');
+const cfg = require('./config');
 
-const APP_VERSION = config.app_version;   // 主版本号 . 子版本号 (功能更新) . 修订版本号（release编译版本号） . dev开发版本号
-const APP_ENV = config.env.NODE_ENV;      // prod-正式, local-本地, test-测试, release-预发布, demo-Mock测试
+const APP_VERSION = cfg.app_version;   // 主版本号 . 子版本号 (功能更新) . 修订版本号（release编译版本号） . dev开发版本号
+const APP_ENV = cfg.env.NODE_ENV;      // prod-正式, local-本地, test-测试, release-预发布, demo-Mock测试
 
 module.exports = ({ onGetWebpackConfig }) => {
   onGetWebpackConfig(config => {
     config.module
       .rule('new-rule')
       .test(/\.scss$/)
-      .use(MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader')
-      .loader(MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader')
+      .use('sass-loader', 'css-loader', 'mini-css')
+      .loader('sass-loader', 'css-loader', MiniCssExtractPlugin.loader)
 
     config.plugin('HtmlWebpackPlugin').use(HtmlWebpackPlugin, [{ template: 'public/index.html' }])
-
-    config.module
-      .rule('scss')
-      .use(MiniCssExtractPlugin.loader)
-      .tap(options => ({ ...options, publicPath: '../' }))
 
     config.plugin('DefinePlugin', [{
       'process.env.APP_VERSION': JSON.stringify(APP_VERSION),
@@ -27,7 +23,7 @@ module.exports = ({ onGetWebpackConfig }) => {
     }])
 
     config.output
-      .path('./')
+      .path(path.resolve(__dirname))
       .filename('js/[name]-[hash].' + APP_ENV + '.v_' + APP_VERSION + '.js',)
       .chunkFilename('js/app_[name]-[chunkhash].' + APP_ENV + '.v_' + APP_VERSION + '.js')
   })
